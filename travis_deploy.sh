e525e3e23fe5e70e12ca972172ce29141bc63979
Roar Skaar <roar.skaar@gmail.com>
2017-11-10 04:08:37 +0100

    chore: travis configuration

---
+++ travis_deploy.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# This script runs on Travis CI and deploy the generated files to gh-pages branch of the repository iff it's triggered
+# by a merge into the master branch
+
+set -ex
+
+USER="rskaar"
+EMAIL="roar.skaar@gmail.com"
+DEPLOY_MESSAGE="Deploy to GitHub Pages (auto)"
+GH_REPO="github.com/turistforeningen/restful-api-guidelines.git"
+
+if [[ "${TRAVIS}" = "true" && "${TRAVIS_SECURE_ENV_VARS}" = "true" && "${TRAVIS_PULL_REQUEST}" = "false" && "${TRAVIS_BRANCH}" = "master" ]]; then
+    echo "Deploying to gh-pages branch"
+    cd output
+    git init
+    git config user.name "${USER}"
+    git config user.email "${EMAIL}"
+    git add -A
+    git commit -m "${DEPLOY_MESSAGE}"
+    git push --force --quiet "https://${GH_TOKEN}@${GH_REPO}" master:gh-pages
+    echo "Deployed successfully to gh-pages branch"
+else
+    echo "It's not an update of the master branch, skipping the deployment"
+fi
